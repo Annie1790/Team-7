b00e87bd0e79807f876ac87eff4c2514daccc266
Annie1790 <budayaniko@outlook.com>
2023-01-14 10:10:52 +0000

    fresh arrivals overlay is working

--- desktop_home/script.js
+++ desktop_home/script.js
@@ -1,13 +1,12 @@
-const priceTag = document.querySelectorAll(".example-class");
-const imageContainer = document.querySelectorAll(".example-image")
-
-
-let product = {
-    name: "product",
-    price: "£59",
-    source: "../images/BW-Jacket.jpg"
-}
-
-function insertProduct() {
-    
-}
+const productItems = 
+    [
+        brownTrousers= {
+            price: "£199",
+            source: "./images/Brown-Trousers.jpg"
+        },
+        BwJacket = {
+            price: "£109",
+            source: "./images/BW-Jacket.jpg"
+        },
+        
+    ]
