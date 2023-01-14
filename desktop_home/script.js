47bba5aefbebb46daa6c96ca1a02dd3650e833d7
Annie1790 <budayaniko@outlook.com>
2023-01-14 09:19:41 +0000

    fresh arrivals section in progress

---
+++ desktop_home/script.js
@@ -0,0 +1,13 @@
+const priceTag = document.querySelectorAll(".example-class");
+const imageContainer = document.querySelectorAll(".example-image")
+
+
+let product = {
+    name: "product",
+    price: "£59",
+    source: "../images/BW-Jacket.jpg"
+}
+
+function insertProduct() {
+    
+}
