47183e2b7486073e3ae843db6155e0bd842e0c61
Annie1790 <budayaniko@outlook.com>
2023-01-14 10:28:46 +0000

    fresh arrivals overlay is working

--- desktop_home/script.js
+++ desktop_home/script.js
@@ -1,12 +1,50 @@
+const productImage = document.querySelectorAll(".product-image");
+const priceTag = document.querySelectorAll(".box-price-tag");
+
 const productItems = 
     [
         brownTrousers= {
             price: "£199",
-            source: "./images/Brown-Trousers.jpg"
+            source: "../images/Brown-Trousers.jpg"
         },
         BwJacket = {
-            price: "£109",
-            source: "./images/BW-Jacket.jpg"
+            price: "£111",
+            source: "../images/BW-Jacket.jpg"
         },
-        
+        greenPufferCoat = {
+            price: "£129",
+            source: "../images/Green-PufferCoat.jpg"
+        },
+        greenTrousers = {
+            price: "£36",
+            source: "../images/Green-Trousers.jpg"
+        },
+        greyBag = {
+            price: "£39",
+            source: "../images/Grey-Bag.jpg"
+        },
+        greyTrousers = {
+            price: "£35",
+            source: "../images/Grey-Trousers.jpg"
+        },
+        maroonDress = {
+            price: "£89",
+            source: "../images/Maroon-Dress.jpg"
+        },
+        greyShirt = {
+            price: "£11",
+            source: "../images/Sitting.jpg"
+        },
+        tanJacket = {
+            price: "£149",
+            source: "../images/Tan-Jacket.jpg"
+        },
+        top = {
+            price: "£24",
+            source: "../images/Top.jpg"
+        },
+        trainers = {
+            price: "£41",
+            source: "../images/Trainers.jpg"
+        }
     ]
