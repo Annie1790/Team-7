5028a894b13bcd14c158dea003f1208ffcec8bfc
Annie1790 <budayaniko@outlook.com>
2023-01-15 19:38:22 +0000

    fresh arrivals js added

--- desktop_home/script.js
+++ desktop_home/script.js
@@ -2,71 +2,85 @@ const productImages = document.querySelectorAll(".product-image");
 const priceTag = document.querySelectorAll(".box-price-tag");
 
 let productItems = [
-        brownTrousers = {
-            price: "£199",
-            source: "../images/Brown-Trousers.jpg",
-            active: false
-        },
-        BwJacket = {
-            price: "£111",
-            source: "../images/BW-Jacket.jpg",
-            active: false
-        },
-        greenPufferCoat = {
-            price: "£129",
-            source: "../images/Green-PufferCoat.jpg",
-            active: false
-        },
-        greenTrousers = {
-            price: "£36",
-            source: "../images/Green-Trousers.jpg",
-            active: false
-        },
-        greyBag = {
-            price: "£39",
-            source: "../images/Grey-Bag.jpg",
-            active: false
-        },
-        greyTrousers = {
-            price: "£35",
-            source: "../images/Grey-Trousers.jpg",
-            active: false
-        },
-        maroonDress = {
-            price: "£89",
-            source: "../images/Maroon-Dress.jpg",
-            active: false
-        },
-        greyShirt = {
-            price: "£11",
-            source: "../images/Sitting.jpg",
-            active: false
-        },
-        tanJacket = {
-            price: "£149",
-            source: "../images/Tan-Jacket.jpg",
-            active: false
-        },
-        top = {
-            price: "£24",
-            source: "../images/Top.jpg",
-            active: false
-        },
-        trainers = {
-            price: "£41",
-            source: "../images/Trainers.jpg",
-            active: false
-        }
-    ];
+    {
+        price: "£199",
+        source: "../images/Brown-Trousers.jpg",
+        active: false
+    },
+    {
+        price: "£111",
+        source: "../images/BW-Jacket.jpg",
+        active: false
+    },
+    {
+        price: "£129",
+        source: "../images/Green-PufferCoat.jpg",
+        active: false
+    },
+    {
+        price: "£36",
+        source: "../images/Green-Trousers.jpg",
+        active: false
+    },
+    {
+        price: "£39",
+        source: "../images/Grey-Bag.jpg",
+        active: false
+    },
+    {
+        price: "£35",
+        source: "../images/Grey-Trousers.jpg",
+        active: false
+    },
+    {
+        price: "£89",
+        source: "../images/Maroon-Dress.jpg",
+        active: false
+    },
+    {
+        price: "£11",
+        source: "../images/Sitting.jpg",
+        active: false
+    },
+    {
+        price: "£149",
+        source: "../images/Tan-Jacket.jpg",
+        active: false
+    },
+    {
+        price: "£24",
+        source: "../images/Top.jpg",
+        active: false
+    }
+];
 
-console.log(productImages);
-let objPrice = Object.values(productItems);
-console.log(objPrice);
+let i = 0;
 
-for (let i = 0; i <= productItems.length; i++) {
-    console.log(productItems[i])
-    productImages.forEach((image) => image.src = productItems)
+function otherLoop() {
+    if (i == 10) {
+        i = 0;
+    } else {
+        getImgSrc(productItems[i].source);
+        getProductPrice(productItems[i].price);
+        console.log(i)
+        i++;
+    }
 }
 
+function getImgSrc(imageSrc) {
+    productImages.forEach((img) => {
+        img.src = imageSrc;
+    })
+}
 
+function getProductPrice(priceText) {
+    priceTag.forEach((price) => {
+        price.innerHTML = priceText;
+    })
+}
 
+setInterval(otherLoop, 5000)
+
+window.onload = function() {
+    otherLoop()
+}
