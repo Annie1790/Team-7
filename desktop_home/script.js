09e0a544de2bee56c6adb9b209be8b4307406e65
Annie1790 <budayaniko@outlook.com>
2023-01-15 21:16:22 +0000

    fresh arrivals slide function added

--- desktop_home/script.js
+++ desktop_home/script.js
@@ -1,86 +1,83 @@
-const productImages = document.querySelectorAll(".product-image");
-const priceTag = document.querySelectorAll(".box-price-tag");
+const img1 = document.querySelector("#image-1");
+const img2 = document.querySelector("#image-2");
+const img3 = document.querySelector("#image-3");
+const img4 = document.querySelector("#image-4");
 
+const price1 = document.querySelector("#price-tag-1");
+const price2 = document.querySelector("#price-tag-2");
+const price3 = document.querySelector("#price-tag-3");
+const price4 = document.querySelector("#price-tag-4");
+
+let i = 0; //variable for looping
+
+//array with object of prices and images sourced from images repository
 let productItems = [
     {
-        price: "£199",
+        price: "£29",
         source: "../images/Brown-Trousers.jpg",
-        active: false
     },
     {
         price: "£111",
         source: "../images/BW-Jacket.jpg",
-        active: false
     },
     {
         price: "£129",
         source: "../images/Green-PufferCoat.jpg",
-        active: false
     },
     {
         price: "£36",
         source: "../images/Green-Trousers.jpg",
-        active: false
     },
     {
         price: "£39",
         source: "../images/Grey-Bag.jpg",
-        active: false
     },
     {
         price: "£35",
         source: "../images/Grey-Trousers.jpg",
-        active: false
     },
     {
         price: "£89",
         source: "../images/Maroon-Dress.jpg",
-        active: false
     },
     {
         price: "£11",
         source: "../images/Sitting.jpg",
-        active: false
     },
     {
         price: "£149",
         source: "../images/Tan-Jacket.jpg",
-        active: false
     },
     {
         price: "£24",
         source: "../images/Top.jpg",
-        active: false
     }
 ];
 
-let i = 0;
-
-function otherLoop() {
+//select the targeted image source
+function getImgSrc(imageSrc, target) {
+    target.src = imageSrc
+}
+//select the targeted price text
+function getProductPrice(priceText, target) {
+    target.innerHTML = priceText;
+}
+//modifies the image and price elements specified to show a new price and image
+function callNextImage(image, price) {
     if (i == 10) {
         i = 0;
-    } else {
-        getImgSrc(productItems[i].source);
-        getProductPrice(productItems[i].price);
-        console.log(i)
-        i++;
     }
+    console.log(i)
+    getImgSrc(productItems[i].source, image);
+    getProductPrice(productItems[i].price, price);
+    i++;
 }
-
-function getImgSrc(imageSrc) {
-    productImages.forEach((img) => {
-        img.src = imageSrc;
-    })
+//calls the function above on all four images and prices
+function loop() {
+    callNextImage(img1, price1);
+    callNextImage(img2, price2);
+    callNextImage(img3, price3);
+    callNextImage(img4, price4);
 }
-
-function getProductPrice(priceText) {
-    priceTag.forEach((price) => {
-        price.innerHTML = priceText;
-    })
-}
-
-setInterval(otherLoop, 5000)
-
-window.onload = function() {
-    otherLoop()
-}
+//calls a function in milliseconds
+setInterval(loop, 5000)
