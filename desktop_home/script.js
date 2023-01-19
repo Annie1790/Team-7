b214da580bd3b246a729a0e2f12410129e66afa8
Annie1790 <budayaniko@outlook.com>
2023-01-19 13:46:10 +0000

    slides are clickable and shows the product name in url

--- desktop_home/script.js
+++ desktop_home/script.js
@@ -8,47 +8,52 @@ const price2 = document.querySelector("#price-tag-2");
 const price3 = document.querySelector("#price-tag-3");
 const price4 = document.querySelector("#price-tag-4");
 
+const box1 = document.querySelector(".box-1");
+const box2 = document.querySelector(".box-2");
+const box3 = document.querySelector(".box-3");
+const box4 = document.querySelector(".box-4");
+
 let i = 0; //variable for looping
 
 //array with object of prices and Images sourced from Images repository
 let productItems = [
-  {
+  brownTrousers = {
     price: "£29",
     source: "../Images/Brown-Trousers.jpg",
   },
-  {
+  bwJacket = {
     price: "£111",
     source: "../Images/BW-Jacket.jpg",
   },
-  {
+  pufferCoat = {
     price: "£129",
     source: "../Images/Green-PufferCoat.jpg",
   },
-  {
+  greenTrousers = {
     price: "£36",
     source: "../Images/Green-Trousers.jpg",
   },
-  {
+  greyBag = {
     price: "£39",
     source: "../Images/Grey-Bag.jpg",
   },
-  {
+  greyTrousers = {
     price: "£35",
     source: "../Images/Grey-Trousers.jpg",
   },
-  {
+  maroonDress = {
     price: "£89",
     source: "../Images/Maroon-Dress.jpg",
   },
-  {
+  tShirt = {
     price: "£11",
     source: "../Images/Sitting.jpg",
   },
-  {
+  tanJacket = {
     price: "£149",
     source: "../Images/Tan-Jacket.jpg",
   },
-  {
+  top = {
     price: "£24",
     source: "../Images/Top.jpg",
   },
@@ -67,7 +72,6 @@ function callNextImage(image, price) {
   if (i == 10) {
     i = 0;
   }
-  console.log(i);
   getImgSrc(productItems[i].source, image);
   getProductPrice(productItems[i].price, price);
   i++;
@@ -82,4 +86,22 @@ function loop() {
 //calls a function in milliseconds
 setInterval(loop, 5000);
 
+
+/******************************************************* */
+//get source string from event
+function getSourceFromDom(e) {
+  const source = e.srcElement.currentSrc.slice(29);
+  return source;
+}
+
+//creates a boxes array
+const boxes = [box1,box2,box3,box4];
+
+//loops through the box and applies the function on each variable
+boxes.forEach((box) => {
+box.addEventListener("click", function(e) {
+  console.log(getSourceFromDom(e))
+  window.location.href = `productpage.html?product=${getSourceFromDom(e)}`
+})
+})
 
