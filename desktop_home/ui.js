7930293e2d470b1555f5c3b248510cdc32cd2241
waterzaddy <kelli.garnett@pm.me>
2023-01-20 17:52:33 +0000

    Added brown trousers images & added descriptions

--- desktop_home/ui.js
+++ desktop_home/ui.js
@@ -1,204 +1,208 @@
 //array with object of prices and Images sourced from Images repository
 let productItems = [
-    {
-        name: "Brown Trousers",
-        price: "£29",
-        images: {
-            primary: "../Images/Brown-Trousers.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+  {
+    name: "Belted Capris Trousers",
+    price: "£39",
+    images: {
+      primary: "../Images/Brown-Trousers.jpg",
+      second: "../Images/Brown-Trousers-2.jpg",
+      third: "../Images/Brown-Trousers-3.jpg",
+      fourth: "../Images/Brown-Trousers-4.jpg",
     },
-    {
-        name: "BW Jacket",
-        price: "£111",
-        images: {
-            primary: "../Images/BW-Jacket.jpg",
-            second: "../Images/BW-Jacket-2.jpg",
-            third: "../Images/BW-Jacket-3.jpg",
-            fourth: "../Images/BW-Jacket-4.jpg"
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+    descriptions: {
+      descr1: "Ellie Tahari brand trousers",
+      descr2: "Mid rise waist",
+      descr3: "Decorative velvet trim belt",
+      descr4: "95% Wool",
+      descr5: "Back pockets",
     },
-    {
-        name: "Puffer Coat",
-        price: "£89.99",
-        images: {
-            primary: "../Images/Green-PufferCoat.jpg",
-            second: "../Images/Green-PufferCoat-2.jpg",
-            third: "../Images/Green-PufferCoat-3.jpg",
-            fourth: "../Images/Green-PufferCoat-4.jpg"
-        },
-        descriptions: {
-            descr1: "Drawstring hood with faux-fur trim",
-            descr2: "Zip fastening",
-            descr3: "Side pockets",
-            descr4: "Faux leather piping",
-            descr5: "Regular fit"
-        }
+  },
+  {
+    name: "Wool Collarless Jacket",
+    price: "£111",
+    images: {
+      primary: "../Images/BW-Jacket.jpg",
+      second: "../Images/BW-Jacket-2.jpg",
+      third: "../Images/BW-Jacket-3.jpg",
     },
-    {
-        name: "Green Trousers",
-        price: "£36",
-        images: {
-            primary: "../Images/Green-Trousers.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+    descriptions: {
+      descr1: "Kasper brand wool jacket",
+      descr2: "74% Wool knit fabric",
+      descr3: "Closes with hook & eye",
+      descr4: "Stand collar",
+      descr5: "Lined interior",
     },
-    {
-        name: "Grey Bag",
-        price: "£39",
-        images: {
-            primary: "../Images/Grey-Bag.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+  },
+  {
+    name: "Hooded Puffer Coat",
+    price: "£59",
+    images: {
+      primary: "../Images/Green-PufferCoat.jpg",
+      second: "../Images/Green-PufferCoat-2.jpg",
+      third: "../Images/Green-PufferCoat-3.jpg",
+      fourth: "../Images/Green-PufferCoat-4.jpg",
     },
-    {
-        name: "Grey Trousers",
-        price: "£35",
-        images: {
-            primary: "../Images/Grey-Trousers.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+    descriptions: {
+      descr2: "Old Navy brand jacket",
+      descr2: "Drawstring hood with faux-fur trim",
+      descr3: "Zip fastening",
+      descr4: "Side pockets",
+      descr5: "Faux leather piping",
     },
-    {
-        name: "Maroon Dress",
-        price: "£89",
-        images: {
-            primary: "../Images/Maroon-Dress.jpg",
-            second: "../Images/Maroon-Dress-2.jpg",
-            third: "../Images/Maroon-Dress-3.jpg"
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+  },
+  {
+    name: "Green Trousers",
+    price: "£36",
+    images: {
+      primary: "../Images/Green-Trousers.jpg",
     },
-    {
-        name: "T-shirt",
-        price: "£11",
-        images: {
-            primary: "../Images/Sitting.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
     },
-    {
-        name: "Tan Jacket",
-        price: "£149",
-        images: {
-            primary: "../Images/Tan-Jacket.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+  },
+  {
+    name: "Grey Bag",
+    price: "£39",
+    images: {
+      primary: "../Images/Grey-Bag.jpg",
     },
-    {
-        name: "Top",
-        price: "£24",
-        images: {
-            primary: "../Images/Top.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
     },
-    {
-        name: "Reversible Jacket",
-        price: "£187",
-        images: {
-            primary: "../Images/Reversible-Jacket-1.jpg",
-            second: "../Images/Reversible-Jacket-2.jpg",
-            third: "../Images/Reversible-Jacket-3.jpg",
-            fourth: "../Images/Reversible-Jacket-4.jpg"
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+  },
+  {
+    name: "Grey Trousers",
+    price: "£35",
+    images: {
+      primary: "../Images/Grey-Trousers.jpg",
     },
-    {
-        name: "Riding Cape",
-        price: "£299",
-        images: {
-            primary: "../Images/Riding-Cape-1.jpg",
-            second: "../Images/Riding-Cape-2.jpg",
-            third: "../Images/Riding-Cape-3.jpg",
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
     },
-    {
-        name: "Valentino Trousers",
-        price: "£1666",
-        images: {
-            primary: "../Images/Valentino-Trousers-1.jpg",
-            second: "../Images/Valentino-Trousers-2.jpg",
-            third: "../Images/Valentino-Trousers-3.jpg",
-            fourth: "../Images/Valentino-Trousers-4.jpg"
-        },
-        descriptions: {
-            descr1: "",
-            descr2: "",
-            descr3: "",
-            descr4: "",
-            descr5: ""
-        }
+  },
+  {
+    name: "Velvent Floral Party Dress",
+    price: "£89",
+    images: {
+      primary: "../Images/Maroon-Dress.jpg",
+      second: "../Images/Maroon-Dress-2.jpg",
+      third: "../Images/Maroon-Dress-3.jpg",
     },
-
+    descriptions: {
+      descr1: "St. Michael brand dress",
+      descr2: "Textured velvet floral pattern",
+      descr3: "Mid length",
+      descr4: "Built in slip",
+      descr5: "Zips up the side",
+    },
+  },
+  {
+    name: "T-shirt",
+    price: "£11",
+    images: {
+      primary: "../Images/Sitting.jpg",
+    },
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
+    },
+  },
+  {
+    name: "Wool Cashmere Jacket",
+    price: "£349",
+    images: {
+      primary: "../Images/Tan-Jacket.jpg",
+      second: "../Images/Tan-Jacket-2.jpg",
+      third: "../Images/Tan-Jacket-3.jpg",
+      fourth: "../Images/Tan-Jacket-4.jpg",
+    },
+    descriptions: {
+      descr1: "Max Mara brand coat",
+      descr2: "Loose oversized fit",
+      descr3: "Wool-cashmere blend fabric",
+      descr4: "Side pockets",
+      descr5: "Fully lined",
+    },
+  },
+  {
+    name: "Long Sleeve Blouse",
+    price: "£24",
+    images: {
+      primary: "../Images/Top.jpg",
+    },
+    descriptions: {
+      descr1: "Loose fitting",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
+    },
+  },
+  {
+    name: "Reversible Hooded Jacket",
+    price: "£187",
+    images: {
+      primary: "../Images/Reversible-Jacket-1.jpg",
+      second: "../Images/Reversible-Jacket-2.jpg",
+      third: "../Images/Reversible-Jacket-3.jpg",
+      fourth: "../Images/Reversible-Jacket-4.jpg",
+    },
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
+    },
+  },
+  {
+    name: "Riding Cape",
+    price: "£299",
+    images: {
+      primary: "../Images/Riding-Cape-1.jpg",
+      second: "../Images/Riding-Cape-2.jpg",
+      third: "../Images/Riding-Cape-3.jpg",
+    },
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
+    },
+  },
+  {
+    name: "Valentino Trousers",
+    price: "£1666",
+    images: {
+      primary: "../Images/Valentino-Trousers-1.jpg",
+      second: "../Images/Valentino-Trousers-2.jpg",
+      third: "../Images/Valentino-Trousers-3.jpg",
+      fourth: "../Images/Valentino-Trousers-4.jpg",
+    },
+    descriptions: {
+      descr1: "",
+      descr2: "",
+      descr3: "",
+      descr4: "",
+      descr5: "",
+    },
+  },
 ];
 
 export default productItems;
